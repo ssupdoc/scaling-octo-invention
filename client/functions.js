2b50d4b1fc9a2a6b823e73c61d690bb395502816
reecebreebaart <reecebreebaart@users.github.com>
2020-09-15 11:45:32 +1200

    Added product display

--- client/functions.js
+++ client/functions.js
@@ -11,7 +11,7 @@ function getResults()
     xhttp.onreadystatechange = function() {
         if (this.readyState ==4 && this.status ==200){
             console.log(this.responseText);
-            //displayResults(this.responseText);
+            displayResults(this.responseText);
         }
     }
     var url = window.location.href + "api/fetch/" + searchTerm;
@@ -24,42 +24,38 @@ function getResults()
 function displayResults(results)
 {
     var resultsArray = JSON.parse(results);
-    var resultsDiv = document.getElementById("results")
+    var resultsDiv = document.getElementById("results");
 
     for (var i=0; i<10; i++)
     {
-        //if new product, create new div
-        if(i = 0 || resultsArray.products[i].product_title!=resultsArray.products[i-1].product_title)
-        {
+       
             //div
             var productDiv = document.createElement("div");
-            productDiv.id = resultsArray.products[i].product_title;
+            productDiv.id = resultsArray[i].product_title;
             //title
             var productTitle = document.createElement("h4");
-            productTitle.innerHTML = resultsArray.products[i].product_title;
+            productTitle.innerHTML = resultsArray[i].product_title;
             productDiv.appendChild(productTitle);
             //average score
             var productRating = document.createElement("div");
             productRating.classList.add("rating");
-            productRating.innerHTML = resultsArray.products[i].average_star_rating;
+            productRating.innerHTML = resultsArray[i].star_rating;
             productDiv.appendChild(productRating);
             //button event
             var productButton = document.createElement("button");
             productButton.classList.add("productButton");
-            productButton.addEventListener("click", getProduct(resultsArray.products[i].product_title))
+            productButton.innerText = "View Reviews";
+            productButton.addEventListener("click", getProduct(resultsArray[i].product_title))
             productDiv.appendChild(productButton);
 
-            resultsDiv.appendChild(productDiv);
-            
-        }
-        
+            resultsDiv.appendChild(productDiv);        
     }
 }
 
 function getProduct(name)
 {
     //backend stuff
-    displayProduct(reviews)
+    //displayProduct(reviews)
 }
 
 function displayProduct(reviews)
